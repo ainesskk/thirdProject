bc86f8960ccc79c50865be0893ee4eafbc424009
ainesskk <ksenia_krr@mail.ru>
2025-01-16 17:16:44 +0300

    fix current location detection

--- src/contexts/WeatherContextProvider.jsx
+++ src/contexts/WeatherContextProvider.jsx
@@ -6,33 +6,47 @@ const WeatherContextProvider = ({ children }) => {
     const [currentCityInfo, setCurrentCityInfo] = useState(null);
 
     useEffect(() => {
-        // navigator.geolocation.getCurrentPosition(function(position) {
-        //     let latitude = position.coords.latitude;
-        //     let longitude = position.coords.longitude;
-        //
-        //     const getWeatherData = async () => {
-        //         const responseDate = await getGeolocationWeather(latitude, longitude);
-        //
-        //         const timestamp = responseDate.sys.sunset;
-        //         const date = new Date(timestamp * 1000);
-        //
-        //         const info = {
-        //             city: responseDate.name,
-        //             temperature: Math.round(responseDate.main.temp - 273.15),
-        //             feelTemperature: responseDate.main.feels_like,
-        //             humidity: responseDate.main.humidity,
-        //             icon: responseDate.weather[0].icon,
-        //             sunset: date,
-        //         }
-        //
-        //         setCurrentCityInfo(info);
-        //     }
-        //
-        //     getWeatherData();
-        // }, function(error) {
-        //     console.error('ошибка получения геолокации', error);
-        // });
-        navigator.geolocation.getCurrentPosition(console.log, console.error);
+        const getIPGeolocation = async () => {
+            try {
+                const response = await fetch('https://ipapi.co/json/');
+                const data = await response.json();
+                return {
+                    latitude: data.latitude,
+                    longitude: data.longitude,
+                };
+            } catch (error) {
+                console.error('Ошибка ipapi:', error);
+                return null;
+            }
+        };
+
+        const getWeatherData = async () => {
+            try {
+                const position = await getIPGeolocation();
+
+                const responseDate = await getGeolocationWeather(position.latitude, position.longitude);
+
+                const timestamp = responseDate.sys.sunset;
+                const date = new Date(timestamp * 1000);
+
+                const info = {
+                    city: responseDate.name,
+                    temperature: Math.round(responseDate.main.temp - 273.15),
+                    feelTemperature: responseDate.main.feels_like,
+                    humidity: responseDate.main.humidity,
+                    icon: responseDate.weather[0].icon,
+                    sunset: date,
+                };
+
+                setCurrentCityInfo(info);
+            } catch (error) {
+                console.error('Получение данных погоды по местоположению:', error);
+            }
+        };
+
+        getWeatherData();
+
+
     }, []);
 
     return (
